76ef906e496082b5342d462081ab59a895743dbb
Vicente Buendia <vicente.buendia@uv.es>
2016-04-21 08:42:09 -0700

    generated Coursera exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+##   A pair of functions to cache the inverse of a matrix, so
+##  when an inverse of a matrix has been solved once, there is no
+##  need to make the calculations again thus saving lots of 
+##  computation time
 
-## Write a short comment describing this function
+
+## makeCacheMatrix generates a list of functions assigned to
+## a given "x" matrix with to:
+## 1.- set: initialize it, attaching it "inver" variable 
+## 2.- get: retrieve "x" matrix values
+## 3.- setinver: assign a value to its internal "inver" variable
+## 4.- getinver: retrieve "inver" value
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inver <- NULL
+      set <- function(y) {
+            x <<- y
+            inver <<- NULL
+      }
+      get <- function() x
+      setinver <- function(solve) inver <<- solve
+      getinver <- function() inver
+      list(set = set, get = get,
+           setinver = setinver,
+           getinver = getinver)
 }
 
 
-## Write a short comment describing this function
+## Making use of makeCacheMatrix, returns inverse of matrix "x"
+## Firs verify if matrix "x" had been previously solved, otherwise
+## solves it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+        # First attempt
+        inverseMat <- x$getinver()
+  
+        # Was the inverse calculated previously? If so, return it
+        if (!is.null(inverseMat)) {
+          message("getting cached data")
+          return(inverseMat)
+        }
+        # inverseMat is null, so it need to be computed
+        message("solving...")
+        data <- x$get()
+        inverseMat <- solve(data)
+        # store it in order to avoid computing it again
+        x$setinver(inverseMat)
+        #... and finally return inverseMat
+        inverseMat
 }
